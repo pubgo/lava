450063a03c6f7c24c56814eba43db883e920403d
白云辉 <kooksee@163.com>
2020-11-21 14:47:21 +0800

    fix: 添加task Message

--- golug_entry/entry.task.go
+++ golug_entry/entry.task.go
@@ -4,9 +4,16 @@ import (
 	"context"
 )
 
+type Message struct {
+	ID        []byte
+	Body      []byte
+	Timestamp int64
+	Attempts  uint16
+}
+
 type TaskOptions struct{}
 type TaskOption func(opts *TaskOptions)
-type TaskHandler func(ctx context.Context, data []byte) error
+type TaskHandler func(ctx context.Context, data *Message) error
 type TaskEntry interface {
 	Entry
 	Register(name string, handler TaskHandler, opts ...TaskOption)
