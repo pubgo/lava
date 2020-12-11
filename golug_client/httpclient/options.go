805ed641ea5d0ab4b8effcc0ef4e64d7dd252cb7
白云辉 <kooksee@163.com>
2020-12-11 22:49:19 +0800

    fix: update

--- golug_client/httpclient/options.go
+++ golug_client/httpclient/options.go
@@ -14,3 +14,31 @@ type Options struct {
 
 // Option ...
 type Option func(opts *Options)
+
+// WithHTTPTimeout sets hystrix timeout
+func WithHTTPTimeout(timeout time.Duration) Option {
+	return func(c *Options) {
+		c.Timeout = timeout
+	}
+}
+
+// WithRetryCount sets the retry count for the client
+func WithRetryCount(retryCount int) Option {
+	return func(c *Options) {
+		c.RetryCount = retryCount
+	}
+}
+
+// WithRetrier sets the strategy for retrying
+func WithRetrier(retrier Retriable) Option {
+	return func(c *Options) {
+		c.Retrier = retrier
+	}
+}
+
+// WithMiddleware sets the strategy for retrying
+func WithMiddleware(m Middleware) Option {
+	return func(c *Options) {
+		c.Middles = append(c.Middles, m)
+	}
+}
--- golug_client/httpclient/retry.go
+++ golug_client/httpclient/retry.go
@@ -17,3 +17,37 @@ type RetriableFunc func(retry int) time.Duration
 func (f RetriableFunc) NextInterval(retry int) time.Duration {
 	return f(retry)
 }
+
+type retrier struct {
+	backoff Backoff
+}
+
+// NewRetrier returns retrier with some backoff strategy
+func NewRetrier(backoff Backoff) Retriable {
+	return &retrier{
+		backoff: backoff,
+	}
+}
+
+// NewRetrierFunc returns a retrier with a retry function defined
+func NewRetrierFunc(f RetriableFunc) Retriable {
+	return f
+}
+
+// NextInterval returns next retriable time
+func (r *retrier) NextInterval(retry int) time.Duration {
+	return r.backoff.Next(retry)
+}
+
+type noRetrier struct {
+}
+
+// NewNoRetrier returns a null object for retriable
+func NewNoRetrier() Retriable {
+	return &noRetrier{}
+}
+
+// NextInterval returns next retriable time, always 0
+func (r *noRetrier) NextInterval(retry int) time.Duration {
+	return 0 * time.Millisecond
+}
