09c2cf7b710c3ef836a3a0baa914be69b252914f
白云辉 <kooksee@163.com>
2021-02-09 14:27:22 +0800

    fix: update

--- client/etcdv3/config.go
+++ client/etcdv3/config.go
@@ -1,11 +1,9 @@
 package etcdv3
 
 import (
-	"bytes"
-	"encoding/json"
 	"time"
 
-	"github.com/pubgo/xerror"
+	"github.com/pubgo/golug/golug_types"
 	"go.etcd.io/etcd/clientv3"
 	"google.golang.org/grpc"
 )
@@ -17,48 +15,26 @@ var DefaultCfg = clientv3.Config{
 	DialOptions: []grpc.DialOption{grpc.WithBlock()},
 }
 
-var _ json.Unmarshaler = (*duration)(nil)
-var _ json.Marshaler = (*duration)(nil)
-
-type duration int64
-
-func (d duration) MarshalJSON() ([]byte, error) {
-	return []byte(time.Duration(d).String()), nil
-}
-
-func (d *duration) UnmarshalJSON(data []byte) error {
-	data = bytes.Trim(data, "\"")
-
-	dur, err := time.ParseDuration(string(data))
-	if err != nil {
-		return xerror.WrapF(err, "data: %s", data)
-	}
-
-	*d = duration(dur)
-
-	return nil
-}
-
 type config struct {
-	Endpoints            []string          `json:"endpoints"`
-	AutoSyncInterval     duration          `json:"interval"`
-	DialTimeout          duration          `json:"timeout"`
-	DialKeepAliveTime    duration          `json:"keepalive"`
-	DialKeepAliveTimeout duration          `json:"keepalive_timeout"`
-	MaxCallSendMsgSize   int               `json:"max_send"`
-	MaxCallRecvMsgSize   int               `json:"max_recv"`
-	Username             string            `json:"username"`
-	Password             string            `json:"password"`
-	DialOptions          []grpc.DialOption `json:"-"`
+	Endpoints            []string             `json:"endpoints"`
+	AutoSyncInterval     golug_types.Duration `json:"interval"`
+	DialTimeout          golug_types.Duration `json:"timeout"`
+	DialKeepAliveTime    golug_types.Duration `json:"keepalive"`
+	DialKeepAliveTimeout golug_types.Duration `json:"keepalive_timeout"`
+	MaxCallSendMsgSize   int                  `json:"max_send"`
+	MaxCallRecvMsgSize   int                  `json:"max_recv"`
+	Username             string               `json:"username"`
+	Password             string               `json:"password"`
+	DialOptions          []grpc.DialOption    `json:"-"`
 }
 
 // 转化为etcd config
 func (t config) EtcdConfig() (cfg clientv3.Config) {
 	cfg.Endpoints = t.Endpoints
-	cfg.AutoSyncInterval = time.Duration(t.AutoSyncInterval)
-	cfg.DialTimeout = time.Duration(t.DialTimeout)
-	cfg.DialKeepAliveTime = time.Duration(t.DialKeepAliveTime)
-	cfg.DialKeepAliveTimeout = time.Duration(t.DialKeepAliveTimeout)
+	cfg.AutoSyncInterval = t.AutoSyncInterval.Duration
+	cfg.DialTimeout = t.DialTimeout.Duration
+	cfg.DialKeepAliveTime = t.DialKeepAliveTime.Duration
+	cfg.DialKeepAliveTimeout = t.DialKeepAliveTimeout.Duration
 	cfg.MaxCallSendMsgSize = t.MaxCallSendMsgSize
 	cfg.MaxCallRecvMsgSize = t.MaxCallRecvMsgSize
 	cfg.Username = t.Username
