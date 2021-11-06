7021a1f19dbbc07aed5d10c792193b9ce86da204
白云辉 <kooksee@163.com>
2021-11-06 10:28:31 +0800

    fix: update

--- internal/example/esbuild/main.go
+++ internal/example/esbuild/main.go
@@ -10,8 +10,18 @@ func main() {
 	var now = time.Now()
 	defer func() { fmt.Println(time.Since(now)) }()
 
-	result := api.Transform("let x: number = 1", api.TransformOptions{
-		Loader: api.LoaderTS,
+	result := api.Transform(`
+import { Trend } from 'k6/metrics';
+
+const myTrend = new Trend('my_trend');
+
+export default function () {
+  myTrend.add(1);
+  myTrend.add(2);
+}
+`, api.TransformOptions{
+		Loader: api.LoaderJS,
+		Format: api.FormatCommonJS,
 	})
 
 	if len(result.Errors) == 0 {
