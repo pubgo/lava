df8bb1ad312948fbc5a81bea95d6ccff6c01327f
百里(barry) <kooksee@163.com>
2022-04-28 23:07:30 +0800

    fix

--- service/running.go
+++ service/running.go
@@ -2,13 +2,13 @@ package service_builder
 
 import (
 	"fmt"
-	"github.com/pubgo/lava/cmd/cmds/vercmd"
 	"os"
 	"sort"
 
 	"github.com/pubgo/xerror"
 	"github.com/urfave/cli/v2"
 
+	"github.com/pubgo/lava/cmd/cmds/vercmd"
 	"github.com/pubgo/lava/runtime"
 	"github.com/pubgo/lava/service"
 	"github.com/pubgo/lava/version"
