802c995db87327fa16abad0e08588a4790970a71
百里(barry) <kooksee@163.com>
2023-02-11 13:12:26 +0800

    fix

--- modules/automaxprocs/builder.go
+++ modules/automaxprocs/builder.go
@@ -1,21 +1,19 @@
 package automaxprocs
 
 import (
-	"github.com/pubgo/dix/di"
 	"github.com/pubgo/funk/assert"
 	"github.com/pubgo/funk/lifecycle"
 	"github.com/pubgo/funk/log"
 	"go.uber.org/automaxprocs/maxprocs"
 )
 
+// doc: https://github.com/KimMachineGun/automemlimit
+
 const name = "automaxprocs"
 
-// https://github.com/KimMachineGun/automemlimit
-func init() {
-	di.Provide(func() lifecycle.Handler {
-		return func(lc lifecycle.Lifecycle) {
-			ff := log.GetLogger(name).WithCallerSkip(2).Info().Msgf
-			assert.Must1(maxprocs.Set(maxprocs.Logger(ff)))
-		}
-	})
+func New() lifecycle.Handler {
+	return func(lc lifecycle.Lifecycle) {
+		ff := log.GetLogger(name).WithCallerSkip(2).Info().Msgf
+		assert.Must1(maxprocs.Set(maxprocs.Logger(ff)))
+	}
 }
--- modules/gcnotifier/builder.go
+++ modules/gcnotifier/builder.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/CAFxX/gcnotifier"
-	"github.com/pubgo/dix/di"
 	"github.com/pubgo/funk/async"
 	"github.com/pubgo/funk/lifecycle"
 	"github.com/pubgo/funk/log"
@@ -13,32 +12,30 @@ import (
 
 var Name = "gc"
 
-func init() {
-	di.Provide(func(log log.Logger) lifecycle.Handler {
-		if !runmode.IsDebug {
-			return nil
-		}
+func New(log log.Logger) lifecycle.Handler {
+	if !runmode.IsDebug {
+		return nil
+	}
 
-		var logs = log.WithName(Name)
-		return func(lc lifecycle.Lifecycle) {
-			lc.AfterStart(func() {
-				var cancel = async.GoCtx(func(ctx context.Context) error {
-					var gc = gcnotifier.New()
-					defer gc.Close()
+	var logs = log.WithName(Name)
+	return func(lc lifecycle.Lifecycle) {
+		lc.AfterStart(func() {
+			var cancel = async.GoCtx(func(ctx context.Context) error {
+				var gc = gcnotifier.New()
+				defer gc.Close()
 
-					// TODO handler
+				// TODO handler
 
-					for {
-						select {
-						case <-gc.AfterGC():
-							logs.Info().Msg("gc notify")
-						case <-ctx.Done():
-							return nil
-						}
+				for {
+					select {
+					case <-gc.AfterGC():
+						logs.Info().Msg("gc notify")
+					case <-ctx.Done():
+						return nil
 					}
-				})
-				lc.BeforeStop(cancel)
+				}
 			})
-		}
-	})
+			lc.BeforeStop(cancel)
+		})
+	}
 }
