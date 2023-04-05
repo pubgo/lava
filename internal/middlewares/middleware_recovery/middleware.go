6b208e879e4eea6963a233d9e404c9bcf48a0c85
kooksee <kooksee@163.com>
2023-04-05 21:03:54 +0800

    fix

--- internal/middlewares/middleware_recovery/middleware.go
+++ internal/middlewares/middleware_recovery/middleware.go
@@ -48,6 +48,13 @@ func New() lava.Middleware {
 				gErr = errutil.ConvertErr2Status(pb).Err()
 			}()
 
+			if v, ok := req.(lava.Validator); ok && v != nil {
+				gErr = v.Validate()
+				if gErr != nil {
+					return nil, gErr
+				}
+			}
+
 			return next(ctx, req)
 		}
 	}
