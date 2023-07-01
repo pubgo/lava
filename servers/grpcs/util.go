dd43e4625ea87cd0f52dabac338f55c34feb2bc8
barry <kooksee@163.com>
2023-07-01 22:45:42 +0800

    fix

--- servers/grpcs/util.go
+++ servers/grpcs/util.go
@@ -11,15 +11,3 @@ func serviceFromMethod(m string) string {
 
 	return strings.Split(strings.Trim(m, "/"), "/")[0]
 }
-
-func parsePath(path string) (string, string, string) {
-	parts := strings.Split(path, "/")
-	if len(parts) < 2 {
-		return "", "", ""
-	}
-
-	method := parts[len(parts)-1]
-	pkgService := parts[len(parts)-2]
-	prefix := strings.Join(parts[0:len(parts)-2], "/")
-	return prefix, pkgService, method
-}
