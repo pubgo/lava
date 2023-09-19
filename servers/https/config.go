27564d26de9cb3b73b7d4ed485399694e240436b
baiyunhui <baiyunhui@asants.com>
2023-09-19 09:41:18 +0800

    refactor

--- servers/https/config.go
+++ servers/https/config.go
@@ -3,20 +3,20 @@ package https
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/pubgo/funk/version"
-	fiber_builder2 "github.com/pubgo/lava/pkg/fiber_builder"
+	"github.com/pubgo/lava/pkg/fiber_builder"
 )
 
 type Config struct {
-	Http       *fiber_builder2.Config `yaml:"http"`
-	Ws         *fiber_builder2.WsCfg  `yaml:"ws"`
-	PrintRoute bool                   `yaml:"print-route"`
-	PathPrefix string                 `yaml:"path-prefix"`
+	Http       *fiber_builder.Config `yaml:"http"`
+	Ws         *fiber_builder.WsCfg  `yaml:"ws"`
+	PrintRoute bool                  `yaml:"print-route"`
+	PathPrefix string                `yaml:"path-prefix"`
 }
 
 func DefaultCfg() Config {
 	return Config{
-		Http:       &fiber_builder2.Config{},
-		Ws:         &fiber_builder2.WsCfg{},
+		Http:       &fiber_builder.Config{},
+		Ws:         &fiber_builder.WsCfg{},
 		PrintRoute: true,
 		PathPrefix: version.Project(),
 	}
