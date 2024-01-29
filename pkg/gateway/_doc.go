f06e046942cb98a8f74c358fbe1f2a5162ebe5ad
kooksee <kooksee@163.com>
2024-01-29 19:02:11 +0800

    fix

--- pkg/gateway/_doc.go
+++ pkg/gateway/_doc.go
@@ -3,3 +3,5 @@ package gateway
 // https://cloud.google.com/service-infrastructure/docs/service-management/reference/rpc/google.api#google.api.DocumentationRule.FIELDS.string.google.api.DocumentationRule.selector
 // https://google.aip.dev/123
 // https://github.com/dgrr/http2
+// https://github.com/r3labs/sse
+// https://github.com/googollee/go-socket.io
