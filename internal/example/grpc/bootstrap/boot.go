fdd47e6f3ccec1d68b374df63ef9b0219f87eff1
kooksee <kooksee@163.com>
2024-01-31 19:48:29 +0800

    fix: add task service

--- internal/example/grpc/bootstrap/boot.go
+++ internal/example/grpc/bootstrap/boot.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pubgo/funk/config"
 	"github.com/pubgo/funk/recovery"
 	"github.com/pubgo/lava/cmds/app"
+	"github.com/pubgo/lava/internal/example/grpc/taskcmd"
 
 	"github.com/pubgo/lava/internal/example/grpc/handlers/gid_handler"
 	"github.com/pubgo/lava/internal/example/grpc/services/gid_client"
@@ -19,6 +20,7 @@ func Main() {
 	di.Provide(gid_handler.NewHttp)
 	di.Provide(gid_handler.NewHttp111)
 	di.Provide(gid_client.New)
+	di.Provide(taskcmd.New)
 
 	app.Run(di)
 }
---
+++ internal/example/grpc/taskcmd/cmd.go
@@ -0,0 +1,47 @@
+package taskcmd
+
+import (
+	"context"
+	"fmt"
+	"github.com/pubgo/dix"
+	"github.com/pubgo/lava/servers/tasks"
+	"github.com/urfave/cli/v3"
+	"time"
+)
+
+func New(di *dix.Dix) *cli.Command {
+	return &cli.Command{
+		Name: "test-task",
+		Action: func(ctx *cli.Context) error {
+			dix.Inject(di, tasks.New(new(service))).Run()
+			return nil
+		},
+	}
+}
+
+type service struct {
+	cancel context.CancelFunc
+}
+
+func (s *service) Start() {
+	var ctx, cancel = context.WithCancel(context.Background())
+	s.cancel = cancel
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			fmt.Println("test task")
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func (s *service) Stop() {
+	s.cancel()
+}
+
+func (s *service) Run() {
+	//TODO implement me
+	panic("implement me")
+}
