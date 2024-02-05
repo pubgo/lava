e928954ccd5a6db0b0dda916d855ef8c2b113fa0
kooksee <kooksee@163.com>
2024-02-05 15:28:00 +0800

    fix

--- internal/example/grpc/bootstrap/boot.go
+++ internal/example/grpc/bootstrap/boot.go
@@ -1,10 +1,13 @@
 package bootstrap
 
 import (
+	"context"
+	"fmt"
 	"github.com/pubgo/funk/config"
 	"github.com/pubgo/funk/recovery"
 	"github.com/pubgo/lava/cmds/app"
 	"github.com/pubgo/lava/internal/example/grpc/taskcmd"
+	"github.com/pubgo/lava/lava"
 
 	"github.com/pubgo/lava/internal/example/grpc/handlers/gid_handler"
 	"github.com/pubgo/lava/internal/example/grpc/services/gid_client"
@@ -21,6 +24,18 @@ func Main() {
 	di.Provide(gid_handler.NewHttp111)
 	di.Provide(gid_client.New)
 	di.Provide(taskcmd.New)
+	di.Provide(func() lava.Middleware {
+		return lava.MiddlewareWrap{
+			Name: "t",
+			Next: func(next lava.HandlerFunc) lava.HandlerFunc {
+				return func(ctx context.Context, req lava.Request) (lava.Response, error) {
+					fmt.Println("global", req.Endpoint())
+					fmt.Printf("%#v\n", req.Payload())
+					return next(ctx, req)
+				}
+			},
+		}
+	})
 
 	app.Run(di)
 }
