e76acafddf24fa5b1e04412133b8fb665b2e9a22
barry <kooksee@163.com>
2024-11-29 22:08:06 +0800

    fix: barry 2024-11-29 22:08:06

--- cmds/protoc-gen-lava/internal/version.go
+++ cmds/protoc-gen-lava/internal/version.go
@@ -1,3 +1,20 @@
 package internal
 
-const version = "v0.0.1"
+import (
+	"fmt"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+const Version = "v0.0.2"
+
+func protocVersion(gen *protogen.Plugin) string {
+	v := gen.Request.GetCompilerVersion()
+	if v == nil {
+		return "(unknown)"
+	}
+	var suffix string
+	if s := v.GetSuffix(); s != "" {
+		suffix = "-" + s
+	}
+	return fmt.Sprintf("v%d.%d.%d%s", v.GetMajor(), v.GetMinor(), v.GetPatch(), suffix)
+}
