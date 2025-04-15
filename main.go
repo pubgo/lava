3da6dc6d98dbb4899d956f7be641cdbdc2c9cb0c
barry <kooksee@163.com>
2025-04-15 23:00:30 +0800

    fix: barry quick fix, 2025-04-15 23:00:30

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 	"github.com/pubgo/funk/assert"
 	"github.com/pubgo/lava/buildtasks"
 	"github.com/pubgo/lava/core/flags"
+	"github.com/thejerf/suture/v4"
 
 	_ "github.com/thejerf/suture/v4"
 )
 
 func main() {
 	goyek.Undefine(workflow.PipelineAll)
+
+	s := suture.NewSimple("")
+	s.Add()
+	s.Remove()
 
 	workflow.StageTest.SetDeps(append(workflow.StageTest.Deps(), buildtasks.GoLint))
 	goyek.SetDefault(goyek.Define(goyek.Task{
