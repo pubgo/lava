dcb28c887f84a701b38237985411d402ba92c9bf
barry <kooksee@163.com>
2025-05-29 22:02:02 +0800

    fix: barry quick fix, 2025-05-29 22:02:02

--- servers/grpcs/config.go
+++ servers/grpcs/config.go
@@ -18,14 +18,9 @@ type Config struct {
 	BaseUrl           string              `yaml:"base_url"`
 	GrpcConfig        *grpcbuilder.Config `yaml:"grpc_config"`
 	EnableCors        bool                `yaml:"enable_cors"`
-	EnablePingPong    bool                `yaml:"enable_ping_pong"`
 
-	// unix seconds
-	PingPongTime int32 `yaml:"ping_pong_time"`
-	GrpcPort     *int  `yaml:"grpc_port"`
-	HttpPort     *int  `yaml:"http_port"`
-
-	WsReadLimit *int `yaml:"ws_read_limit"`
+	GrpcPort *int `yaml:"grpc_port"`
+	HttpPort *int `yaml:"http_port"`
 }
 
 func defaultCfg() *Config {
