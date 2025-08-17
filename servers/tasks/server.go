3bdc1ccc5ab2914bce3f37769f7c93c98b287c4c
barry <kooksee@163.com>
2025-08-17 12:43:40 +0800

    fix: barry quick fix, 2025-08-17 12:43:40

--- servers/tasks/server.go
+++ servers/tasks/server.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/pubgo/funk/assert"
-	"github.com/pubgo/funk/async"
 	"github.com/pubgo/funk/errors"
 	"github.com/pubgo/funk/errors/errutil"
 	"github.com/pubgo/funk/generic"
@@ -50,38 +49,26 @@ type Server struct {
 	conf       *Config
 }
 
-func (s *Server) String() string {
-	return "task"
-}
-
 func (s *Server) Serve(ctx context.Context) error {
 	defer func() {
-		<-ctx.Done()
-
 		logutil.LogOrErr(s.log, "[http-debug-server] Shutdown", func() error {
 			return s.httpServer.ShutdownWithTimeout(time.Second * 5)
 		})
 	}()
 
 	httpLn := assert.Exit1(net.Listen("tcp", fmt.Sprintf(":%d", generic.FromPtr(s.conf.HttpPort))))
-	logutil.OkOrFailed(s.log, "service starts", func() error {
-		async.GoDelay(func() error {
-			s.log.Info().Msg("[http-debug-server] Server Starting")
-			logutil.LogOrErr(s.log, "[http-debug-server] Server Stop", func() error {
-				defer recovery.Exit()
-				if err := s.httpServer.Listener(httpLn); err != nil &&
-					!errors.Is(err, http.ErrServerClosed) &&
-					!errors.Is(err, net.ErrClosed) {
-					return err
-				}
-				return nil
-			})
-			return nil
-		})
-
+	s.log.Info().Msg("[http-debug-server] Server Starting")
+	logutil.LogOrErr(s.log, "[http-debug-server] Server Stop", func() error {
+		defer recovery.Exit()
+		if err := s.httpServer.Listener(httpLn); err != nil &&
+			!errors.Is(err, http.ErrServerClosed) &&
+			!errors.Is(err, net.ErrClosed) {
+			return err
+		}
 		return nil
 	})
 
+	<-ctx.Done()
 	return nil
 }
 
