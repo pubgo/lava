68986b4bda7f0bcd14ba8e99b0ee36204a7bcea2
barry <kookshine@gmail.com>
2025-08-18 13:06:00 +0800

    fix: barry@feat/supervisor, 2025-08-18 13:06:00

--- core/scheduler/interface.go
+++ core/scheduler/interface.go
@@ -3,6 +3,8 @@ package scheduler
 import (
 	"context"
 	"time"
+
+	"github.com/reugn/go-quartz/quartz"
 )
 
 type JobFunc func(ctx context.Context, name string, metadata *JobMetadata) error
@@ -15,4 +17,15 @@ type JobRegistry interface {
 	Once(name string, delay time.Duration, fn JobFunc)
 	Every(name string, dur time.Duration, fn JobFunc)
 	Cron(name, expr string, fn JobFunc)
+}
+
+type Job struct {
+	Name     string
+	Delay    *time.Duration
+	Dur      *time.Duration
+	CronExpr string
+	Metadata JobMetadata
+	ExecErr  error
+	JobKey   *quartz.JobKey
+	Location *time.Location
 }
