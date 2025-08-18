bf65ad8a396c391ccc92e1350167d1c548446355
barry <kookshine@gmail.com>
2025-08-18 13:12:48 +0800

    fix: barry@feat/supervisor, 2025-08-18 13:12:48

--- core/scheduler/interface.go
+++ core/scheduler/interface.go
@@ -19,6 +19,35 @@ type JobRegistry interface {
 	Cron(name, expr string, fn JobFunc)
 }
 
+type JobManager interface {
+	Add(name string, spec JobSpec, fn JobFunc)
+	Pause(name string, spec JobSpec, fn JobFunc)
+	Resume(name string, spec JobSpec, fn JobFunc)
+	Delete(name string, spec JobSpec, fn JobFunc)
+	Start(name string, spec JobSpec, fn JobFunc)
+	Stop(name string, spec JobSpec, fn JobFunc)
+	List(name string, spec JobSpec, fn JobFunc)
+	Get(name string, spec JobSpec, fn JobFunc)
+}
+
+type JobSpec struct {
+	Once  *OnceJob
+	Every *EveryJob
+	Cron  *CronJob
+}
+
+type OnceJob struct {
+	Delay time.Duration
+}
+
+type EveryJob struct {
+	Dur time.Duration
+}
+
+type CronJob struct {
+	Expr string
+}
+
 type Job struct {
 	Name     string
 	Delay    *time.Duration
