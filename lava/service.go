f5518cda21f66621682b38081386683bee8c8856
barry <kooksee@163.com>
2025-08-27 22:35:30 +0800

    fix: barry quick fix, 2025-08-27 22:35:30

--- lava/service.go
+++ lava/service.go
@@ -15,18 +15,6 @@ type Close interface {
 	Close(ctx context.Context) error
 }
 
-//type Server interface {
-//	fmt.Stringer
-//
-//	// Serve starts the server, no async.
-//	Serve(ctx context.Context) error
-//}
-
-//type Service interface {
-//	Start(ctx context.Context) error
-//	Stop(ctx context.Context) error
-//}
-
 // Listener provides an interface for starting and stopping the server.
 type Listener interface {
 	Listen(context.Context, net.Listener) error
