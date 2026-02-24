7460203af15a6fedf7b2e5d0087ac67b63016e6a
barry <kookshine@gmail.com>
2026-02-24 13:49:04 +0800

    chore: quick update fix/router at 2026-02-24 13:49:04

--- servers/grpcs/server.go
+++ servers/grpcs/server.go
@@ -89,7 +89,7 @@ func (s *serviceImpl) init(
 	grpcRouters []lava.GrpcRouter,
 	httpRouters []lava.HttpRouter,
 	grpcHttpRouters []lava.GrpcHttpRouter,
-	// grpcProxy []lava.GrpcProxy,
+// grpcProxy []lava.GrpcProxy,
 	dixMiddlewares []lava.Middleware,
 	metric metrics.Metric,
 	log log.Logger,
@@ -216,14 +216,16 @@ func (s *serviceImpl) init(
 	grpcGatewayApiPrefix := "/api"
 	log.Info().Msgf("service gateway base path: %s", grpcGatewayApiPrefix)
 
-	for _, m := range mux.GetRouteMethods() {
-		log.Info().
-			Str("operation", m.Operation).
-			Any("rpc-meta", lo.FromPtr(mux.GetOperation(m.Operation)).Meta).
-			Str("verb", m.Verb).
-			Any("path-vars", m.Vars).
-			Str("extras", fmt.Sprintf("%v", m.Extras)).
-			Msgf("grpc gateway router info: %s %s", m.Method, "/"+strings.Trim(grpcGatewayApiPrefix, "/")+m.Path)
+	if conf.EnablePrintRouter {
+		for _, m := range mux.GetRouteMethods() {
+			log.Info().
+				Str("operation", m.Operation).
+				Any("rpc-meta", lo.FromPtr(mux.GetOperation(m.Operation)).Meta).
+				Str("verb", m.Verb).
+				Any("path-vars", m.Vars).
+				Str("extras", fmt.Sprintf("%v", m.Extras)).
+				Msgf("grpc gateway router info: %s %s", m.Method, "/"+strings.Trim(grpcGatewayApiPrefix, "/")+m.Path)
+		}
 	}
 
 	httpServer.Use("/debug", debug.App())
